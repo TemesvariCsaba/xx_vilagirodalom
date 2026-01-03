f81d39f3186fd8c8f155093cfabbb68742e9a656
csaba <temesvari.csaba_2022@bolyai-szakkozep.hu>
2026-01-03 20:08:43 +0100

    console col tablazat

---
+++ colspanjs/index.js
@@ -0,0 +1,9 @@
+// 1. feladat 1. commit
+console.log("| Szerző       | Mű                                | Fogalmak  |           |") // Fejlec kiiratasa konzolra
+console.log("| Appolliniare | A megsebzett galamb és a szökőkút | képvers   | emlékezés |") //elso sor kiiratasa konzolra
+console.log("| Appolliniare | Búcsú                             | avantgárd |           |") //masodik sor kiiratasa konzolra
+console.log("| Thomas Mann  | Mario és a varázsló               | kisregény |           |") //harmadik sor kiiratasa konzolra
+console.log("| Franz Kafka  | A per                             | regény    |           |") //negyedik sor kiiratasa konzolra
+console.log("| Franz Kafka  | Az átváltozás                     | kisregény | groteszk  |") //otodik sor kiiratasa konzolra
+
+
