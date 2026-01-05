a9afd5373bc7458a947a2f9f4804d2589e29efab
csaba <temesvari.csaba_2022@bolyai-szakkozep.hu>
2026-01-05 20:49:32 +0100

    row html double esemeny impl

--- rowspanhtml/index.js
+++ rowspanhtml/index.js
@@ -33,3 +33,57 @@ htmlNormalButton.addEventListener("click", function(){ // kattintas eseten rahiv
     testTrow.appendChild(testFogalom) // hozzafuzi a sorhoz
 
 })
+
+/** @type {HTMLButtonElement} a dupla sorok gombja*/
+
+const htmlDoubleButton = document.getElementById("htmlbuttondouble") //elkeri azonosito alapjan a dupla sor gombjat
+htmlDoubleButton.addEventListener("click", function(){ // kattintas eseten rahivja a gombra az esemenykezelot
+    /** @type {RowType} uj sor adatait tartalmazo valtozo tipusa */
+    const testDoubleRow = { //adatok eltarolasa egy objektumban
+        author: "Új szerző", //szerzo oszlop tuajdonsaganak erteke
+        title1: "Új mű", //mu oszlop tulajdonsaganak erteke
+        concepts1: "Új fogalom", //fogalmak oszlop tulajdonsaganak erteke
+        title2: "Másik műve", // azonos szerzohoz tartozo masik mu
+        concepts2: "Másik fogalma" // masik muhoz tartozo fogalom
+    }
+    /** @type {HTMLTableSectionElement} a torzs amihez csatolom az uj sort */
+    const htmlTbody = document.getElementById("tablebody") // elkeri a torzset azonosito alapjan
+
+    /**@type {HTMLTableRowElement} az uj sor */
+    const testTrow = document.createElement("tr") //letrehoz egy uj sort
+    htmlTbody.appendChild(testTrow) //hozzafuzi a sort a torzshoz
+
+    /** @type {HTMLTableColElement} az uj cella*/
+    const testSzerzo = document.createElement("td") // letrehozza a szerzo cellat
+    testSzerzo.innerText = testDoubleRow.author // beallitja az erteket 
+    testTrow.appendChild(testSzerzo) // hozzafuzi a sorhoz
+
+     /** @type {HTMLTableColElement} az uj cella*/
+    const testMu = document.createElement("td") // letrehozza a mu cellat
+    testMu.innerText = testDoubleRow.title1 // beallitja az erteket 
+    testTrow.appendChild(testMu) // hozzafuzi a sorhoz
+
+     /** @type {HTMLTableColElement} az uj cella*/
+    const testFogalom = document.createElement("td") // letrehozza a fogalom cellat
+    testFogalom.innerText = testDoubleRow.concepts1 // beallitja az erteket 
+    testTrow.appendChild(testFogalom) // hozzafuzi a sorhoz
+
+    if(testDoubleRow.concepts2&&testDoubleRow.title2){ // ha definialva van masik muve a szerzonek es annak a fogalma
+        testSzerzo.rowSpan = "2" // a szerzo cella ket sort fog elfoglalni
+        /** @type {HTMLTableRowElement}  az uj sor */
+        const testTrowDouble = document.createElement("tr") // letrehoz egy uj sort 
+        htmlTbody.appendChild(testTrowDouble) // hozzafuzi a torzshoz
+
+         /** @type {HTMLTableColElement} az uj cella*/
+        const testMuSecond = document.createElement("td") // letrehozza a mu cellat
+        testMuSecond.innerText = testDoubleRow.title2 // beallitja az erteket 
+        testTrowDouble.appendChild(testMuSecond) // hozzafuzi a sorhoz
+
+        /** @type {HTMLTableColElement} az uj cella*/
+        const testFogalomSecond = document.createElement("td") // letrehozza a fogalom cellat
+        testFogalomSecond.innerText = testDoubleRow.concepts2 // beallitja az erteket 
+        testTrowDouble.appendChild(testFogalomSecond) // hozzafuzi a sorhoz
+    }
+    
+
+})
