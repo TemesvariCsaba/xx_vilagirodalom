f2de27b846592d18d8ba4b4dea993d4e7737ac15
csaba <temesvari.csaba_2022@bolyai-szakkozep.hu>
2026-01-05 15:53:45 +0100

    console row fuggveny

--- rowspanjs/index.js
+++ rowspanjs/index.js
@@ -1,7 +1,8 @@
 /** @type {string[]} Fejlec tomb tipusa */
 const headerArr = ["Szerző", "Mű", "Fogalmak"] //fejlec tartalma egy tombben
 
-/** @type {{author: string, title1: string, concepts1: string, title2?: string,  concepts2?: string}[]} body tomb tipusa*/
+/** @typedef {{author: string, title1: string, concepts1: string, title2?: string,  concepts2?: string}} BodyArr tipus definicio amit hasznalni fogok a tombokre */
+/** @type {BodyArr[]} a body tomb tipusa  */
 const bodyArr = [ // body tartalma egy tombben
     {
         author: "Appolliniare", // Appolliniare sor szerzo tulajdonsaga
@@ -23,15 +24,21 @@ const bodyArr = [ // body tartalma egy tombben
         concepts2: "kisregény" // masodik Franz Kafka sor fogalmak tulajdonsaga
     }
 ]
-console.log("| "+headerArr[0]+" | "+headerArr[1]+" | "+headerArr[2]+" | ") //fejlec sor konzolra kiiratasa
-for(const element of bodyArr){ //vegigjarom a tombot egy ciklus segitsegevel
-    /** @type {string} valtozo tipusa */
-    let createRows = "| "+element.author+" | "+element.title1+" | "+element.concepts1+" | " // beleteszi azokat a tulajdonsagokat a valtozoba amik fixen vannak
-if (element.title2 && element.concepts2){ // ha definialva van a masik cim es fogalmak cella akkor 
-    createRows+= "\n"+"|_    |"+element.title2+" | "+element.concepts2+" | " //letrehoz egy uj sort es hozzaadja a valtozohoz a tulajdonsagok ertekeit
+/**
+ *  eltarolja a tablazatot meghivaskor meg kiirja a console-ra 
+ * @param {BodyArr[]} array array parameter tipusa
+ * @returns {void} nincs visszateresi erteke
+ */
+function generateConsoleTable(array) {//tomb parameteru fuggveny
+    console.log("| "+headerArr[0]+" | "+headerArr[1]+" | "+headerArr[2]+" | ") //fejlec sor konzolra kiiratasa
+    for(const element of array){ //vegigjarom a tombot egy ciklus segitsegevel
+        /** @type {string} valtozo tipusa */
+        let createRows = "| "+element.author+" | "+element.title1+" | "+element.concepts1+" | " // beleteszi azokat a tulajdonsagokat a valtozoba amik fixen vannak
+    if (element.title2 && element.concepts2){ // ha definialva van a masik cim es fogalmak cella akkor 
+        createRows+= "\n"+"|_    |"+element.title2+" | "+element.concepts2+" | " //letrehoz egy uj sort es hozzaadja a valtozohoz a tulajdonsagok ertekeit
+    }
+    console.log(createRows) //tablazat torzsenek a kiiratasa 
+    }
 }
-console.log(createRows) //tablazat torzsenek a kiiratasa 
-}
-
-
+generateConsoleTable(bodyArr) //Teljes tablazat kiirasa a console-ra
 
